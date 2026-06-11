f85322932f23d4dca9cba495208e5230079d32c9
boyangqin <qboyang@gmail.com>
2026-06-11 08:04:24 -0700

    refactor a little

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,6 +13,7 @@ import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.routing.*
+import org.h2.tools.Server
 
 @PWA(name = "Project Base for Vaadin", shortName = "Project Base")
 @StyleSheet(Lumo.STYLESHEET)
@@ -23,6 +24,9 @@ class AppShell : AppShellConfigurator
  * Run this function to launch your app in Embedded Jetty.
  */
 fun main() {
+    // 0. Start H2 Console
+    Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082").start()
+    println("H2 Console is running at http://localhost:8082")
     DatabaseConfig.init() // Initialize the database
 
     // 1. Start Ktor API asynchronously on port 8081
@@ -35,9 +39,11 @@ fun main() {
             mainApiRoutes()
         }
     }.start(wait = false)
+    println("Ktor embedded server is running at http://localhost:8081")
 
     // 2. Start Vaadin UI asynchronously on port 8080
     VaadinBoot().apply {
         setPort(8080) // Configures the port
     }.run()           // Starts the embedded server
+    println("Vaadin server is running at http://localhost:8080 - This line is not printed in IntelliJ console")
 }
--- src/main/kotlin/com/example/karibudsl/Database.kt
+++ src/main/kotlin/com/example/karibudsl/Database.kt
@@ -9,8 +9,7 @@ import org.jetbrains.exposed.sql.transactions.transaction
 object DatabaseConfig {
     fun init() {
         // 1. 在连接数据库之前，启动 H2 Web 控制台（端口设为 8082）
-        Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082").start()
-        println("H2 Console is running at http://localhost:8082")
+
 
         // 2. 连接到内存数据库
         Database.connect(
